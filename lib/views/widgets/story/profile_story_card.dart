5550152f21cb71fec3085bd1e866338c8d4dce5e
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-05-10 17:12:14 +0300

    IR-50: Add profile story card class

---
+++ lib/views/widgets/story/profile_story_card.dart
@@ -0,0 +1,10 @@
+class ProfileStoryCard extends StatelessWidget {
+  const ProfileStoryCard({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
