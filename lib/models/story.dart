5be415f5eae51d751981def9103171f07b7e2346
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-05-11 16:55:53 +0300

    IR-49: Add story model;

--- lib/models/story.dart
+++ lib/models/story.dart
@@ -1,1 +1,17 @@
-class S
+import 'package:flutter/material.dart';
+
+import 'model.dart';
+
+class Story extends Model {
+  String _title;
+  double _progress;
+  Color _color;
+  int _flippedPages;
+  double _readingTime;
+
+  get title => _title;
+  get progress => _progress;
+  get color => _color;
+  get flippedPages => _flippedPages;
+  get readingTime => _readingTime;
+}
