f4932c83aa22d659984cb29bbf2ff816f2361678
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-05-11 19:56:47 +0300

    IR-49: Add properties for story width and spacing

--- lib/views/widgets/opened_library/stories_section.dart
+++ lib/views/widgets/opened_library/stories_section.dart
@@ -5,10 +5,21 @@ import 'package:iread_flutter/views/widgets/story/story_card.dart';
 class StoriesSection extends StatelessWidget {
   final List<Story> _stories;
   final String _title;
+  final double _storyWidth;
+  final double _horizontalSpace;
+  final double _verticalSpace;
 
-  StoriesSection({storiesList, @required title})
+  StoriesSection(
+      {storiesList,
+      @required title,
+      storyWidth,
+      horizontalSpace,
+      verticalSpace})
       : _stories = storiesList,
-        _title = title;
+        _title = title,
+        _storyWidth = storyWidth ?? 150,
+        _horizontalSpace = horizontalSpace ?? 24,
+        _verticalSpace = verticalSpace ?? 24;
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +33,12 @@ class StoriesSection extends StatelessWidget {
             style: Theme.of(context).textTheme.headline4,
           ),
           SizedBox(
-            height: 24,
+            height: 32,
           ),
           Wrap(
             alignment: WrapAlignment.center,
-            runSpacing: 32,
-            spacing: 24,
+            runSpacing: _verticalSpace,
+            spacing: _horizontalSpace,
             children: _storiesWidgets(context),
           )
         ],
@@ -43,7 +54,7 @@ class StoriesSection extends StatelessWidget {
     List<Widget> stories = [];
     _stories.forEach((element) {
       final story = Container(
-        width: 150,
+        width: _storyWidth,
         child: StoryCard(
           title: element.title,
           imageUrl: element.imageUrl,
