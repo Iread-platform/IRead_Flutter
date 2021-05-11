b21d9e7e596c9b5ac53e22ac0686c0acdf30e3bb
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-05-11 16:58:13 +0300

    IR-49: Add stories section constructor

--- lib/views/widgets/opened_library/stories_section.dart
+++ lib/views/widgets/opened_library/stories_section.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:iread_flutter/models/story.dart';
 
 class StoriesSection extends StatelessWidget {
+  final List<Story> _stories;
+  final String _title;
+
+  StoriesSection({storiesList, @required title})
+      : _stories = storiesList,
+        _title = title;
+
   @override
   Widget build(BuildContext context) {
     return Container();
