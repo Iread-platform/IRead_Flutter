29f97fb044dfaa087ff9d4c4432086b900b3e325
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-05-16 20:05:42 +0300

    IR-62: Construct color scheme object

---
+++ lib/themes/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ColorScheme colorScheme = ColorScheme(
+  primary: Color(0x845ec2), 
+  primaryVariant: Color(0x500e74), 
+  secondary: Color(0xff5e78), 
+  secondaryVariant: Colr(0x9e1d22), 
+  surface: Colors.white, 
+  background: Colors.white10, 
+  error: Colors.redAccent, 
+  onPrimary: Colors.white,
+  onSecondary: Colors.white, 
+  onSurface: Color(0x845ec2), 
+  onBackground: Colors.white, 
+  onError: Colors.white, brightness: Brightness.light)
