5ff8021ba700d683f0ba4415dbf1c83fdba78c66
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-05-16 20:13:54 +0300

    IR-62: Add theme

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 ColorScheme colorScheme = ColorScheme(
-  primary: Color(0x845ec2), 
-  primaryVariant: Color(0x500e74), 
-  secondary: Color(0xff5e78), 
-  secondaryVariant: Colr(0x9e1d22), 
-  surface: Colors.white, 
-  background: Colors.white10, 
-  error: Colors.redAccent, 
-  onPrimary: Colors.white,
-  onSecondary: Colors.white, 
-  onSurface: Color(0x845ec2), 
-  onBackground: Colors.white, 
-  onError: Colors.white, brightness: Brightness.light)
+    primary: Color(0xff845ec2),
+    primaryVariant: Color(0xff500e74),
+    secondary: Color(0xffff5e78),
+    secondaryVariant: Color(0xff9e1d22),
+    surface: Colors.white,
+    background: Colors.white10,
+    error: Colors.redAccent,
+    onPrimary: Colors.white,
+    onSecondary: Colors.white,
+    onSurface: Color(0xff845ec2),
+    onBackground: Colors.white,
+    onError: Colors.white,
+    brightness: Brightness.light);
---
+++ lib/themes/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:iread_flutter/themes/colors.dart';
+
+ThemeData theme = ThemeData(
+    primaryColor: colorScheme.primary,
+    primarySwatch: Colors.indigo,
+    colorScheme: colorScheme);
