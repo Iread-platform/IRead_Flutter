48f522d66dc19a46b2c9c88187ef82a6b926a40a
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-05-16 21:07:50 +0300

    Ir-64: update fontWeight for headings

--- lib/themes/textTheme.dart
+++ lib/themes/textTheme.dart
@@ -15,15 +15,15 @@ TextTheme textTheme = TextTheme(
   subtitle1: TextStyle(
       color: Colors.black87, fontSize: 18, fontWeight: FontWeight.w600),
   headline6: TextStyle(
-      color: colorScheme.primary, fontSize: 18, fontWeight: FontWeight.bold),
+      color: colorScheme.primary, fontSize: 18, fontWeight: FontWeight.w700),
   headline5: TextStyle(
-      color: colorScheme.primary, fontSize: 20, fontWeight: FontWeight.bold),
+      color: colorScheme.primary, fontSize: 20, fontWeight: FontWeight.w700),
   headline4: TextStyle(
-      color: colorScheme.primary, fontSize: 24, fontWeight: FontWeight.bold),
+      color: colorScheme.primary, fontSize: 24, fontWeight: FontWeight.w700),
   headline3: TextStyle(
-      color: colorScheme.primary, fontSize: 28, fontWeight: FontWeight.bold),
+      color: colorScheme.primary, fontSize: 28, fontWeight: FontWeight.w700),
   headline2: TextStyle(
-      color: colorScheme.primary, fontSize: 32, fontWeight: FontWeight.bold),
+      color: colorScheme.primary, fontSize: 32, fontWeight: FontWeight.w700),
   headline1: TextStyle(
-      color: colorScheme.primary, fontSize: 36, fontWeight: FontWeight.bold),
+      color: colorScheme.primary, fontSize: 36, fontWeight: FontWeight.w700),
 );
