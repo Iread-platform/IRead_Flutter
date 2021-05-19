ff6439d143a0c78f9aa0d9ee9ba7b4653e817cc8
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-05-19 15:01:02 +0300

    IR-72 Fix ResponsiveLayoutBuilder's warnings

--- lib/views/widgets/layout/responsive_layout_builder.dart
+++ lib/views/widgets/layout/responsive_layout_builder.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 
 class ResponsiveLayoutBuilder extends StatelessWidget {
-  Widget Function(BuildContext) _onXSm;
-  Widget Function(BuildContext) _onSm;
-  Widget Function(BuildContext) _onMd;
-  Widget Function(BuildContext) _onLg;
-  Widget Function(BuildContext) _onXLg;
-  Widget Function(BuildContext) _onXXLg;
+  final Widget Function(BuildContext) _onXSm;
+  final Widget Function(BuildContext) _onSm;
+  final Widget Function(BuildContext) _onMd;
+  final Widget Function(BuildContext) _onLg;
+  final Widget Function(BuildContext) _onXLg;
+  final Widget Function(BuildContext) _onXXLg;
 
   /// A wrapper widget that helps to writes a customizable responsive design,
   /// This widget uses the bootstrap's standard web breakpoints.
@@ -36,14 +36,14 @@ class ResponsiveLayoutBuilder extends StatelessWidget {
       Widget Function(BuildContext) onMd,
       Widget Function(BuildContext) onLg,
       Widget Function(BuildContext) onXLg,
-      Widget Function(BuildContext) onXXLg}){
-    _onXSm = onXSm;
-    _onSm = onSm ?? _onXSm;
-    _onMd = onMd ?? _onSm;
-    _onLg = onLg ?? _onMd;
-    _onXLg = onXLg ?? _onLg;
-    _onXXLg = onXXLg ?? _onXLg;
-  }
+      Widget Function(BuildContext) onXXLg})
+      : _onXSm = onXSm,
+        _onSm = onSm ?? onXSm,
+        _onMd = onMd ?? onSm ?? onXSm,
+        _onLg = onLg ?? onMd ?? onSm ?? onXSm,
+        _onXLg = onXLg ?? onLg ?? onMd ?? onSm ?? onXSm,
+        _onXXLg = onXXLg ?? onXLg ?? onLg ?? onMd ?? onSm ?? onXSm;
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
