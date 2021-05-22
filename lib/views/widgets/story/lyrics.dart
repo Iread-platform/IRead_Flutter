9c0617db2ec25926f69cd1e64e9101b7e75f599c
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-05-23 01:45:11 +0300

    IR-68: Add lyrics widget

--- lib/views/widgets/story/lyrics.dart
+++ lib/views/widgets/story/lyrics.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:iread_flutter/models/lyrics/lyrics_word.dart';
 
 class Lyrics extends StatefulWidget {
+  final List<LyricsWord> _lyricsWord;
+
+  Lyrics({@required lyricsWord}) : _lyricsWord = lyricsWord;
+
   @override
   _LyricsState createState() => _LyricsState();
 }
