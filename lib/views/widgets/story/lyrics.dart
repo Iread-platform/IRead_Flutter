dc8dac79ef4cf4d9f2f8bd405152d48e5280290c
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-05-23 01:40:50 +0300

    IR-68: Lyrics widget

---
+++ lib/views/widgets/story/lyrics.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Lyrics extends StatefulWidget {
+  @override
+  _LyricsState createState() => _LyricsState();
+}
+
+class _LyricsState extends State<Lyrics> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
