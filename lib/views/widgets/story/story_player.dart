7b59ab2f9115b5be59e4b588f68926892a3560b2
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-05-22 10:40:10 +0300

    IR-68: Add story player widget

---
+++ lib/views/widgets/story/story_player.dart
@@ -0,0 +1,11 @@
+class StoryPlayer extends StatefulWidget {
+  @override
+  _StoryPlayerState createState() => _StoryPlayerState();
+}
+
+class _StoryPlayerState extends State<StoryPlayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
