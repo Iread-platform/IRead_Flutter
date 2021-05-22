32fae7297f0197fab0278827b58d55c2b08719fb
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-05-23 00:49:45 +0300

    IR-68: Read lyrics from file then convert to lyricsWord list

--- lib/bloc/story_player_bloc.dart
+++ lib/bloc/story_player_bloc.dart
@@ -1,3 +1,24 @@
+import 'package:iread_flutter/models/lyrics/lyrics_word.dart';
+import 'package:iread_flutter/utils/file_utils.dart';
+
 class StoryPlayerBloc {
-  static Future<Map<String, dynamic>> readLyricsFile(String path) async {}
+  static Future<List<LyricsWord>> readLyricsFile(String path) async {
+    final json = await FileUtils.readAssetJson(path);
+
+    return _jsonToLyricsList(json);
+  }
+
+  static List<LyricsWord> _jsonToLyricsList(Map<String, dynamic> json) {
+    List<LyricsWord> lyricsList = [];
+    json = json['lyrics'];
+
+    for (int i = 0; i < json.length; i++) {
+      LyricsWord lyricsWord = LyricsWord(
+          word: json[i]['word'], milliseconds: json[i]['milliseconds']);
+
+      lyricsList.add(lyricsWord);
+    }
+
+    return lyricsList;
+  }
 }
