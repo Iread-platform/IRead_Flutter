3c9f8d032eef07bc007aaf3b0eb4d465ded29d62
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-06-06 12:56:33 +0300

    IR-87: Initiate stories search list in main

--- lib/views/Screens/stories_search_list.dart
+++ lib/views/Screens/stories_search_list.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 class StoriesSearchList extends StatelessWidget {
-  const StoriesSearchList({Key? key}) : super(key: key);
+  const StoriesSearchList({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
