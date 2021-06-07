98a1e41ff9b5d358321cf68a24c93fcc4621228e
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-06-07 16:19:00 +0300

    IR-85: Add rating bar widget

---
+++ lib/views/widgets/review/rating_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RatingBar extends StatelessWidget {
+  final double _rating;
+
+  const RatingBar({double rating, Key key})
+      : _rating = rating,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
