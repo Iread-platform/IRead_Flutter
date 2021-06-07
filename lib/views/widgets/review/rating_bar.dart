89b608aec3a12ed2e8a47552ecb72ed81e2c3626
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-06-07 16:20:59 +0300

    IR-85: Extract rating bar widget

--- lib/views/widgets/review/rating_bar.dart
+++ lib/views/widgets/review/rating_bar.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:iread_flutter/utils/i_read_icons.dart';
 
 class RatingBar extends StatelessWidget {
   final double _rating;
 
-  const RatingBar({double rating, Key key})
+  const RatingBar({@required double rating, Key key})
       : _rating = rating,
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return RatingBarIndicator(
+      rating: 3.75,
+      itemCount: 5,
+      itemSize: 24,
+      unratedColor: Colors.black12,
+      itemPadding: const EdgeInsets.symmetric(horizontal: 2, vertical: 4),
+      itemBuilder: (BuildContext context, int index) => Icon(
+        IReadIcons.star,
+        color: Colors.amberAccent,
+      ),
+    );
   }
 }
--- lib/views/widgets/story/story_details_actions.dart
+++ lib/views/widgets/story/story_details_actions.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:iread_flutter/models/story.dart';
-import 'package:iread_flutter/utils/i_read_icons.dart';
+import 'package:iread_flutter/views/widgets/review/rating_bar.dart';
 import 'package:iread_flutter/views/widgets/story/story_image.dart';
 
 class StoryDetailsActions extends StatelessWidget {
@@ -25,18 +24,7 @@ class StoryDetailsActions extends StatelessWidget {
                 width: 150,
                 child: Column(
                   children: [
-                    RatingBarIndicator(
-                      rating: 3.75,
-                      itemCount: 5,
-                      itemSize: 24,
-                      unratedColor: Colors.black12,
-                      itemPadding: const EdgeInsets.symmetric(
-                          horizontal: 2, vertical: 4),
-                      itemBuilder: (BuildContext context, int index) => Icon(
-                        IReadIcons.star,
-                        color: Colors.amberAccent,
-                      ),
-                    ),
+                    RatingBar(rating: 3.75),
                     StoryImage(imageUrl: _story.imageUrl, color: _story.color)
                   ],
                 ),
