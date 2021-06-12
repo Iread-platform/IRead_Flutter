68367e2013a8089cb38ea0345d77c91b805dad00
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-06-12 13:45:08 +0300

    IR-87: Make main repo a singleton

--- lib/repo/main_repo.dart
+++ lib/repo/main_repo.dart
@@ -1,1 +1,10 @@
-class MainRepo {}
+import 'package:iread_flutter/services/api_service.dart';
+
+class MainRepo {
+  static final MainRepo _instance = MainRepo._internal();
+
+  final ApiService apiService = ApiService();
+
+  factory MainRepo() => _instance;
+  MainRepo._internal();
+}
