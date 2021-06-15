75b3ed993ac6c309c9db53896ad40c2170d6b39c
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-06-15 11:05:42 +0300

    IR-87: Update story modal

--- lib/models/story.dart
+++ lib/models/story.dart
@@ -5,29 +5,47 @@ import 'model.dart';
 class Story extends Model {
   String _title;
   String _imageUrl;
+  DateTime _releaseDate;
+  String _desciption;
+  int _storyLevel;
+  String _writer;
   double _progress;
   Color _color;
   int _flippedPages;
   double _readingTime;
+  double _rating;
 
   Story(
-      {@required title,
+      {@required String title,
       @required color,
-      imageUrl,
-      progress,
-      flippedPages,
-      readingTime,
+      DateTime releaseDate,
+      String description,
+      int storyLevel,
+      String writer,
+      String imageUrl,
+      double progress,
+      int flippedPages,
+      double readingTime,
       id})
       : _title = title,
         _color = color,
+        _releaseDate = releaseDate,
+        _desciption = description,
+        _storyLevel = storyLevel,
+        _writer = writer,
         _imageUrl = imageUrl,
         _flippedPages = flippedPages,
         _readingTime = readingTime,
         _progress = progress,
         super(id: id);
 
-  Story.fromJson(Map<String, dynamic> json) : super(id: json['id']) {
+  Story.fromJson(Map<String, dynamic> json) : super(id: json['storyId']) {
     _title = json['title'];
+    _releaseDate = DateTime.parse(json['releaseDate']);
+    _desciption = json['description'];
+    _storyLevel = json['storyLevel'];
+    _writer = json['writer'];
+    _rating = json['rating'];
     _color = Color(int.parse('0x' + json['color'], radix: 16));
     _imageUrl = json['imageUrl'];
     _flippedPages = json['flippedPages'];
