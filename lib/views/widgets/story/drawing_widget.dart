71c181176376a36c442494d55996371332bc9a0a
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-06-17 23:50:17 +0300

    IR-86: Fill painted shape

--- lib/views/widgets/story/drawing_widget.dart
+++ lib/views/widgets/story/drawing_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DrawingWidget extends StatefulWidget {
@@ -10,8 +11,8 @@ class DrawingWidget extends StatefulWidget {
 }
 
 class _DrawingWidgetState extends State<DrawingWidget> {
-  List<TouchPoints> points = [];
-  StrokeCap strokeType = StrokeCap.butt;
+  List<Offset> points = [];
+  StrokeCap strokeType = StrokeCap.round;
   Color color = Colors.black;
   double opacity = 0.5;
   double strokeWidth = 2.0;
@@ -23,10 +24,15 @@ class _DrawingWidgetState extends State<DrawingWidget> {
     return Stack(
       children: [
         CustomPaint(
-            size: Size.infinite, painter: FingerPainter(pointsList: points)),
+            size: Size.infinite,
+            painter: FingerPainter(points: points, closed: closed)),
         GestureDetector(
           onPanStart: (details) {
-            if (closed) return;
+            if (closed) {
+              points.clear();
+              closed = false;
+            }
+
             setState(
               () {
                 RenderBox renderBox = context.findRenderObject();
@@ -46,7 +52,7 @@ class _DrawingWidgetState extends State<DrawingWidget> {
             setState(() {
               RenderBox renderBox = context.findRenderObject();
               closed = true;
-              addPoint(renderBox, points[0].points);
+              addPoint(renderBox, points[0]);
             });
           },
         )
@@ -55,19 +61,13 @@ class _DrawingWidgetState extends State<DrawingWidget> {
   }
 
   void addPoint(RenderBox renderBox, Offset offset) {
-    points.add(TouchPoints(
-        points: renderBox.globalToLocal(offset),
-        paint: Paint()
-          ..strokeCap = strokeType
-          ..isAntiAlias = true
-          ..color = color.withOpacity(opacity)
-          ..strokeWidth = strokeWidth));
+    points.add(renderBox.globalToLocal(offset));
   }
 
   void _checkDistance(RenderBox renderBox) {
     final length = points.length;
-    Offset start = points[0].points;
-    Offset end = points[length - 1].points;
+    Offset start = points[0];
+    Offset end = points[length - 1];
 
     final distance = (end - start).distance;
 
@@ -79,36 +79,65 @@ class _DrawingWidgetState extends State<DrawingWidget> {
 }
 
 class FingerPainter extends CustomPainter {
-  final List<TouchPoints> pointsList;
+  final List<Offset> pointsList;
   final List<Offset> offsetPoints = [];
+  Paint paintData;
+  bool closed = false;
 
-  FingerPainter({this.pointsList});
+  FingerPainter({@required List<Offset> points, Paint paint, bool closed})
+      : pointsList = points,
+        paintData = paint ?? Paint()
+          ..style = PaintingStyle.fill
+          ..strokeCap = StrokeCap.butt
+          ..isAntiAlias = true
+          ..color = Colors.black12.withOpacity(0.8),
+        closed = closed;
 
   @override
   void paint(Canvas canvas, Size size) {
+    closed ? drawPolygon(canvas, size) : _drawLines(canvas, size);
+  }
+
+  void _drawLines(Canvas canvas, Size size) {
     for (int i = 0; i < pointsList.length - 1; i++) {
       if (pointsList[i] != null && pointsList[i + 1] != null) {
         //Drawing line when two consecutive points are available
-        canvas.drawLine(pointsList[i].points, pointsList[i + 1].points,
-            pointsList[i].paint);
+        canvas.drawLine(
+            pointsList[i],
+            pointsList[i + 1],
+            Paint()
+              ..color = paintData.color
+              ..style = paintData.style
+              ..strokeCap = paintData.strokeCap
+              ..strokeWidth = paintData.strokeWidth
+              ..isAntiAlias = paintData.isAntiAlias);
       } else if (pointsList[i] != null && pointsList[i + 1] == null) {
         offsetPoints.clear();
-        offsetPoints.add(pointsList[i].points);
-        offsetPoints.add(Offset(
-            pointsList[i].points.dx + 0.1, pointsList[i].points.dy + 0.1));
+        offsetPoints.add(pointsList[i]);
+        offsetPoints
+            .add(Offset(pointsList[i].dx + 0.1, pointsList[i].dy + 0.1));
 
         //Draw points when two points are not next to each other
-        canvas.drawPoints(PointMode.points, offsetPoints, pointsList[i].paint);
+        canvas.drawPoints(
+            PointMode.points,
+            offsetPoints,
+            Paint()
+              ..color = paintData.color
+              ..style = paintData.style
+              ..strokeCap = paintData.strokeCap
+              ..strokeWidth = paintData.strokeWidth
+              ..isAntiAlias = paintData.isAntiAlias);
       }
     }
   }
 
+  void drawPolygon(Canvas canvas, Size size) {
+    final Path path = Path();
+
+    path.addPolygon(pointsList, true);
+    canvas.drawPath(path, paintData);
+  }
+
   @override
   bool shouldRepaint(FingerPainter oldDelegate) => true;
-}
-
-class TouchPoints {
-  Paint paint;
-  Offset points;
-  TouchPoints({this.points, this.paint});
 }
