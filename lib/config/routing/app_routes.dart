32e395daa36bb588452f16aaa2c27bf114f5cb8c
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-06-26 13:26:10 +0300

    IR-116: Add fetch story event

--- lib/config/routing/app_routes.dart
+++ lib/config/routing/app_routes.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iread_flutter/bloc/base/base_bloc.dart';
 import 'package:iread_flutter/bloc/search_stories_by_tag_bloc/search_stories_by_tag.dart';
 import 'package:iread_flutter/bloc/search_stories_by_tag_bloc/search_stories_by_tag_event.dart';
+import 'package:iread_flutter/bloc/story/story_details_bloc/story_details_bloc.dart';
 import 'package:iread_flutter/main.dart';
 import 'package:iread_flutter/views/Screens/stories_search_list.dart';
 import 'package:iread_flutter/views/widgets/story/story_assignment_card.dart';
@@ -29,6 +30,13 @@ class AppRoutes {
                   create: (_) => SearchStoriesByTagBloc(LoadingState())
                     ..add(SearchStoriesByTagEvent(params['tag'][0])),
                   child: StoriesSearchList(),
-                )))
+                ))),
+    AppRoute(
+        '/story/:id ',
+        Handler(
+            handlerFunc: (BuildContext context, Map<String, dynamic> params) =>
+                BlocProvider(
+                    create: (context) =>
+                        StoryDetailsBloc(InitialState()).add(LoadingS))))
   ];
 }
