cf85af39834e9133c0b09ca0a8edc51214e76a45
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-07-03 16:02:08 +0300

    IR-31: Add dummy save polygon func to interaction repo

--- lib/repo/interaction_repo.dart
+++ lib/repo/interaction_repo.dart
@@ -1,7 +1,15 @@
 import 'package:iread_flutter/repo/main_repo.dart';
+import 'package:iread_flutter/utils/data.dart';
 
 class InteractionRepo extends MainRepo {
   static InteractionRepo _instance = InteractionRepo._internal();
   factory InteractionRepo() => _instance;
   InteractionRepo._internal();
+
+  final String baseEndpoint = 'interaction';
+  final String savePolygonEndpoint = 'polygon/add';
+
+  Data savePolygon() {
+    return Data.success(1);
+  }
 }
