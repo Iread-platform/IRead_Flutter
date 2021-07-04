edd61d812e9cee0b669d0f424b11aba15888b1f1
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-07-04 10:37:24 +0300

    IR-31: Add attachment repo

---
+++ lib/repo/attachment_repo.dart
@@ -0,0 +1,8 @@
+import 'package:iread_flutter/services/api_service.dart';
+import 'package:iread_flutter/utils/data.dart';
+
+class AttachmentRepo {
+  ApiService _apiService = ApiService();
+
+  Future<Data> saveAudio(String path) {}
+}
