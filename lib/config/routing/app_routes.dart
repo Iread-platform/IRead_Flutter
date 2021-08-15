2642103f38b90d34d5e69b4fefceb5d722a4d6a7
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-08-15 14:58:28 +0300

    IR-152: Show example on how to use auth handler

--- lib/config/routing/app_routes.dart
+++ lib/config/routing/app_routes.dart
@@ -16,16 +16,19 @@ import 'package:iread_flutter/views/widgets/story/story_assignment_card.dart';
 
 class AppRoutes {
   static final List<AppRoute> appRoutes = [
-    // ignore: todo
     // TODO remove dummy route
     AppRoute(
       '/profileStory',
       Handler(
-        handlerFunc: (BuildContext context, Map<String, List<String>> params) =>
-            StoryAssignmentCard(
-          story: story,
-        ),
-      ),
+          handlerFunc:
+              (BuildContext context, Map<String, List<String>> params) =>
+                  // Example on route handler guard
+                  _routeAuthHandler(
+                    context,
+                    StoryAssignmentCard(
+                      story: story,
+                    ),
+                  )),
     ),
     AppRoute(
         '/searchByTag/:tag',
