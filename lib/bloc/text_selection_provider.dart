ff926152c2d64142e7e40cff018ede63086370cd
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-08-17 11:20:29 +0000

    IR-136 Update TextSelectionProvider to provide streams for selected text.

--- lib/bloc/text_selection_provider.dart
+++ lib/bloc/text_selection_provider.dart
@@ -1,15 +1,32 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rxdart/subjects.dart';
 
 class TextSelectionProvider with ChangeNotifier {
   bool textSelectedIsWord = true;
   bool highlighted = false;
   bool playButton = true;
   ScrollController scrollController = new ScrollController();
-  
+  StreamController<TextSelection> _textSelectionStreamController =
+      BehaviorSubject<TextSelection>();
+  StreamController<String> _selectedTextStreamController =
+      BehaviorSubject<String>();
+
+  Stream<String> get selectedTextStream => _selectedTextStreamController.stream;
+  Stream<TextSelection> get textSelectionStream =>
+      _textSelectionStreamController.stream;
+
   TextSelection selection;
-  void changeSelection({selection, textSelected}) {
+  void changeSelection({TextSelection selection, textSelected}) {
     this.selection = selection;
+    if (selection != null) {
+      _textSelectionStreamController.sink.add(selection);
+    }
+    if (textSelected != null) {
+      _selectedTextStreamController.sink.add(textSelected);
+    }
     try {
       List<String> wordSelected = textSelected.split(" ");
       print(wordSelected);
@@ -21,5 +38,12 @@ class TextSelectionProvider with ChangeNotifier {
     } catch (e) {}
 
     notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _selectedTextStreamController.close();
+    _textSelectionStreamController.close();
+    super.dispose();
   }
 }
