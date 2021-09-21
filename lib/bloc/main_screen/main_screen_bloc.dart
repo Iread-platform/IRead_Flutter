0a861e834114a3468de2b79cde14067ede0b5a7e
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-09-21 21:47:17 +0300

    IR-165: Fetch main screen data

--- lib/bloc/main_screen/main_screen_bloc.dart
+++ lib/bloc/main_screen/main_screen_bloc.dart
@@ -1,6 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iread_flutter/bloc/base/base_bloc.dart';
+import 'package:iread_flutter/bloc/main_screen/main_screen_bloc_events.dart';
+import 'package:iread_flutter/bloc/main_screen/main_screen_bloc_state.dart';
 
 class MainScreenBloc extends Bloc<BlocEvent, BlocState> {
   MainScreenBloc(BlocState initialState) : super(initialState);
+
+  @override
+  Stream<BlocState> mapEventToState(BlocEvent event) async* {
+    switch (event.runtimeType) {
+      case FetchMainScreenDataEvent:
+        yield fetchMainScreenData();
+    }
+  }
+
+  MainScreenDataFetchedState fetchMainScreenData() {
+    return MainScreenDataFetchedState();
+  }
 }
