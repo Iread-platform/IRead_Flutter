38b0e3dd6c1ad65b415e76d4fe058807d3acc30a
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-10-09 21:52:07 +0300

    Upload avatar and store it.

--- lib/repo/attachment_repo.dart
+++ lib/repo/attachment_repo.dart
@@ -9,7 +9,7 @@ class AttachmentRepo {
 
   final String baseUrl = 'http://217.182.250.236:5014/api/iread/Attachment/add';
   final String avatarAddEndPoint =
-      'http://217.182.250.236:5014/api/iread/Avatar/add';
+      'http://217.182.250.236:5014/api/iread/Attachment/add';
 
   /// Upload audio then store uploading data in the [UploadingFile] model.
   Future<UploadingFile> saveFile(String path, int storyId) async {
